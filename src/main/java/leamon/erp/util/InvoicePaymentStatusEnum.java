1a503f344db220847a3f01f773c40554bb3e6dad
Mishra, Manish <manish.mishra@team.neustar>
2017-12-22 01:40:47 +0530

    added SELECT_STATUS("--Payment

--- src/main/java/leamon/erp/util/InvoicePaymentStatusEnum.java
+++ src/main/java/leamon/erp/util/InvoicePaymentStatusEnum.java
@@ -2,6 +2,7 @@ package leamon.erp.util;
 
 public enum InvoicePaymentStatusEnum {
 	//CLEAR, REMAINING, NOTHING_PAID
+	SELECT_STATUS("--Payment Status--"),
 	NOTHING_PAID("NOTHING_PAID"),
 	PARTIAL_PAID("PARTIAL_PAID"),
 	ALL_CLEAR("CLEAR");
