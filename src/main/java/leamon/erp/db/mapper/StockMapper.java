e8342c03a4bd410b3ba82a4bec5887685e4acaab
Mishra, Manish <manish.mishra@team.neustar>
2017-12-28 00:51:58 +0530

    added getStockItemQuantity()

--- src/main/java/leamon/erp/db/mapper/StockMapper.java
+++ src/main/java/leamon/erp/db/mapper/StockMapper.java
@@ -5,13 +5,17 @@ import java.util.List;
 import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Many;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.One;
 
+import leamon.erp.model.AccountInfo;
 import leamon.erp.model.StockItem;
+import leamon.erp.model.StockItemQuantity;
 
 /**
  * @author Leamon India
@@ -51,8 +55,6 @@ public interface StockMapper {
 	      
 	})
 	public List<StockItem> getAll() throws Exception;
-	
-	
 
 	@Update(update)
 	void update(StockItem stock) throws Exception;
@@ -67,4 +69,38 @@ public interface StockMapper {
 	@Options(useGeneratedKeys = true, keyProperty = "id")
 	void insert(StockItem stockItem) throws Exception;
 	
+	/* 3.3.1 DEC 24,2017*/
+	
+	@Select("SELECT * FROM STOCK_ITEM_QUANTITY WHERE STOCK_ITEM_ID = #{ID} AND ISENABLE = TRUE")
+	   @Results(value = {
+	      @Result(property = "id", column = "ID"),
+	      @Result(property = "stokItemid", column = "STOCK_ITEM_ID"),
+	      @Result(property = "quantity", column = "QUANTITY"),
+	      @Result(property = "description", column = "DESCRIPTION"),
+	      @Result(property = "isEnable", column = "ISENABLE"),
+	      @Result(property = "createdDate", column = "CREATEDDATE"),
+	      @Result(property = "lastUpdatedDate", column = "LASTUPDATEDDATE")
+	      
+	})
+	public StockItemQuantity getStockItemQuantity() throws Exception;
+	
+	@Select("SELECT * FROM STOCK_ITEM WHERE ISENABLE = TRUE ORDER BY ID")
+	   @Results(value = {
+	      @Result(property = "id", column = "ID"),
+	      @Result(property = "name", column = "NAME"),
+	      @Result(property = "productCode", column = "PRODUCTCODE"),
+	      @Result(property = "size", column = "SIZE"),
+	      @Result(property = "unit", column = "UNIT"),
+	      @Result(property = "finish", column = "FINISH"),
+	      @Result(property = "shape", column = "SHAPE"),
+	      @Result(property = "saleunit", column = "SALE_UNIT"),
+	      @Result(property = "description", column = "DESCRIPTION"),
+	      @Result(property = "imagePath", column = "IMAGEPATH"),
+	      @Result(property = "isEnable", column = "ISENABLE"),
+	      @Result(property = "createdDate", column = "CREATEDDATE"),
+	      @Result(property = "lastUpdatedDate", column = "LASTUPDATEDDATE"),
+	      @Result(property = "stockItemQuantity", javaType=StockItemQuantity.class, column = "ID"
+	      , one =@One(select="getStockItemQuantity"))
+	})
+	public List<StockItem> getAllWithQuantity() throws Exception;
 }
