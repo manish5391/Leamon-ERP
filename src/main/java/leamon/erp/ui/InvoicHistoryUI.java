89e7a46e6214f7034dab8e51fe8739b3ecd55992
Mishra, Manish <manish.mishra@team.neustar>
2017-12-28 00:55:40 +0530

    improving autoacountinfo suggestor and adding opening balance

---
+++ src/main/java/leamon/erp/ui/InvoicHistoryUI.java
@@ -0,0 +1,33 @@
+package leamon.erp.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+
+public class InvoicHistoryUI extends JInternalFrame {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InvoicHistoryUI frame = new InvoicHistoryUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InvoicHistoryUI() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
