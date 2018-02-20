e0e3ffbaeebe41fa8c9edf6abdb924e813c58a22
Mishra, Manish <manish.mishra@team.neustar>
2018-02-20 23:07:15 +0530

    added TABLE_STATE_CITY mouse click

--- src/main/java/leamon/erp/ui/event/MouseClickHandler.java
+++ src/main/java/leamon/erp/ui/event/MouseClickHandler.java
@@ -36,6 +36,8 @@ public class MouseClickHandler implements MouseListener{
 				}else if (table.getName().equals(LeamonERPConstants.TABLE_PAYMENT_RECEIVED_SUMMARY)){
 					/*payment ui - open invoice*/
 					LeamonERP.paymentReceivedUI.openInvoice("View");
+				}else if(LeamonERPConstants.TABLE_STATE_CITY.equals(table.getName())){ /*Release 3.6*/
+					LeamonERP.stateCityManagerUI.getBtnView().doClick();
 				}
 			}
 		}else{
