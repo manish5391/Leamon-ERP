30c6c0ee28abfa02bd9fdb13732841562f84515c
Mishra, Manish <manish.mishra@team.neustar>
2018-03-08 01:54:09 +0530

    TablePaymentReceivedHistoryModel for paymentUiManager

---
+++ src/main/java/leamon/erp/ui/model/TablePaymentReceivedHistoryModel.java
@@ -0,0 +1,112 @@
+package leamon.erp.ui.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+import javax.swing.table.AbstractTableModel;
+
+import org.apache.log4j.Logger;
+
+import com.google.common.base.Strings;
+
+import leamon.erp.model.PaymentReceivedInfo;
+import leamon.erp.model.InvoiceItemInfo;
+import leamon.erp.model.OpeningBalanceInfo;
+import leamon.erp.ui.PaymentUI;
+import leamon.erp.util.ERPEnum;
+import leamon.erp.util.LeamonERPConstants;
+import leamon.erp.util.PaymentEnum;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TablePaymentReceivedHistoryModel extends AbstractTableModel{
+
+	private static final Logger LOGGER = Logger.getLogger(TablePaymentReceivedHistoryModel.class);
+	private static final String CLASS_NAME = "TablePaymentReceivedHistoryModel";
+
+	private final String [] columnName = new String[] {
+			LeamonERPConstants.TABLE_HEADER_SNO,
+			LeamonERPConstants.TABLE_HEADER_PARTY_NAME,
+			LeamonERPConstants.TABLE_HEADER_AMOUNT,
+			LeamonERPConstants.TABLE_HEADER_TYPE,
+			LeamonERPConstants.TABLE_HEADER_DATE,
+			LeamonERPConstants.TABLE_HEADER_DESC
+	};
+
+	private List<PaymentReceivedInfo> paymentReceivedInfos;
+	private List<Integer> sno;
+	
+	public TablePaymentReceivedHistoryModel(List<PaymentReceivedInfo> paymentReceivedInfos){
+		if(paymentReceivedInfos == null){
+			paymentReceivedInfos = new ArrayList<PaymentReceivedInfo>();
+		}
+		this.paymentReceivedInfos = paymentReceivedInfos;
+	}	
+
+	public TablePaymentReceivedHistoryModel(GenericModelWithSnp<List<PaymentReceivedInfo>, PaymentReceivedInfo> genericModelWithSnp){
+		this.paymentReceivedInfos  = genericModelWithSnp.getOb();
+		this.sno = genericModelWithSnp.getSno();
+	}
+
+	@Override
+	public int getRowCount() {
+		if(paymentReceivedInfos == null){
+			return 0;
+		}
+		
+		return paymentReceivedInfos.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return columnName.length;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+
+		final String METHOD_NAME = "getValueAt";
+		Object temp = null;
+		switch (columnIndex) {
+			case 0: temp = /*paymentReceivedInfos.get(rowIndex).getId()*/ sno.get(rowIndex); break;
+			case 1: temp = paymentReceivedInfos.get(rowIndex).getAccountInfo().getName(); break;
+			case 2: temp = paymentReceivedInfos.get(rowIndex).getReceivedPayment(); break;
+			case 3: {
+				if(ERPEnum.TYPE_PAYMENT_WITH_BILL.name().equals(paymentReceivedInfos.get(rowIndex).getType()) ){
+					temp = PaymentEnum.B;
+				}else if(ERPEnum.TYPE_PAYMENT_WITHOUT_BILL.name().equals(paymentReceivedInfos.get(rowIndex).getType()) ){
+					temp = PaymentEnum.W;
+				}else{
+					temp = "N/A";
+				}
+			} break;
+			case 4: temp = paymentReceivedInfos.get(rowIndex).getReceivedDate();break;
+			case 5: temp = paymentReceivedInfos.get(rowIndex).getRemark(); break;
+		}
+		
+		return temp;
+	}
+
+	@Override
+	public String getColumnName(int col) { 
+		return columnName[col]; 
+	} 
+
+	@Override
+	public Class<?> getColumnClass(int columnIndex) {
+		switch(columnIndex){
+		case 0: return Integer.class;
+		case 1: return String.class;
+		case 2: return String.class;
+		case 3: return String.class;
+		case 4: return String.class;
+		case 5: return String.class;
+		default : 
+			return Object.class;
+		}
+	}
+
+}
